d77d6aa1dbf91154f2946539967350826f6fe1b7
pratiklavhaleplp <102862714+pratiklavhaleplp@users.noreply.github.com>
2023-05-11 14:14:21 +0530

    objects functions arrays

--- Object-OrientedJS/2. Objects/sampleSnippets.js
+++ Object-OrientedJS/2. Objects/sampleSnippets.js
@@ -1,12 +1,17 @@
-let userInfo ={
-    name: {
-        firstName: "pratik",
-        lastName: "lavhale"
-    },
-    address: "gadge nagar near radhakrishna temple amravati.",
-    age: "27"
-    }
+function test(){
+    console.log("testing");
+}
+console.log("test function toString : ", test.toString());
 
-    let {address : userAddress, age: userAge} = userInfo;
-    console.log('address is : ', userAddress);
-    console.log('age is : ', userAge);
+let userInfo = {
+    name: "pratik lavhale",
+    say: function sayMyName(who){
+        console.log(who + " is calling " + " My name is : " + this.name);
+        return 0;
+    }
+}
+
+console.log(userInfo.say('rutvik'));
+let testObj = {name : "rutvik"};
+userInfo.say.call(testObj, "cooler wala");
+userInfo.say.apply(testObj, ["cooler wala"]);
